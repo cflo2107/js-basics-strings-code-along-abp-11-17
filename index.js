5a121ebe53cd3b5d62fa6081b195a1d7474b54ec
Cristina Florig <cflo2107@gmail.com>
2017-11-14 23:23:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code in this file!
 const currentUser = 'Cristina Florig';
 
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!``;
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
